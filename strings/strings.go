e412490ec14edeca63bda9b925615b469518b2db
Louch2010 <1175889450@qq.com>
2016-09-21 20:09:17 +0800

    aa

--- strings/strings.go
+++ strings/strings.go
@@ -12,7 +12,7 @@ func (this StringUtil) TrimToEmpty(str string) string {
 	return strings.TrimSpace(str)
 }
 
-//判断是否为空，当为nil或空串时返回false，否则返回true
+//判断是否为空，当为nil或空串时返回false， 否则返回true
 func (this StringUtil) IsEmpty(str string) bool {
 	return len(this.TrimToEmpty(str)) == 0
 }
